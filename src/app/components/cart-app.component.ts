eda754ec8283577a92b669bbc511098bc6bfcf56
ale94 <alejandrorua111@gmail.com>
2024-08-04 22:29:42 -0300

    fix: component cartapp folder reorganization

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../services/product.service';
-import { Product } from '../../models/products';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/products';
 
 @Component({
   selector: 'cart-app',
