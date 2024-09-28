a2450e7f97749203f7d5fcc924074dac15b40a49
ale94 <alejandrorua111@gmail.com>
2024-09-28 18:14:27 -0300

    feat: onAddCart method created

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -22,6 +22,23 @@ export class CartAppComponent implements OnInit {
   }
 
   onAddCart(product: Product) {
-    this.items = [...this.items, { product: { ...product }, quantity: 1 }];
+    const hasItem = this.items.find((item) => item.product.id === product.id);
+    if (hasItem) {
+      this.items = this.items.map((item) => {
+        if (item.product.id === product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1,
+          };
+        }
+        return item;
+      });
+    } else {
+      this.items = [...this.items, { product: { ...product }, quantity: 1 }];
+    }
+  }
+
+  onDeleteCart(id: number) {
+    this.items = this.items.filter((item) => item.product.id !== id);
   }
 }
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
@@ -8,6 +8,10 @@ import { CartItem } from '../../models/cartItem';
   templateUrl: './cart.component.html',
 })
 export class CartComponent {
-
   @Input() items: CartItem[] = [];
+  @Output() idProductEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCart(id: number) {
+    this.idProductEventEmitter.emit(id);
+  }
 }
